57cfa63524e3f56ca4e6e2b84da16416363abce5
jsancs <javivisan19@gmail.com>
2024-08-02 19:43:28 +0200

    apply wrappers to ppo

--- src/ppo/train.py
+++ src/ppo/train.py
@@ -1,46 +1,18 @@
 import gym
-import numpy as np
 import cv2
-from gym.spaces import Box
 import os
 
 from tqdm import trange
 
-from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import RIGHT_ONLY
 
+from src.wrappers import apply_wrappers
 from PPO import PPO
 
 
 IMG_HEIGHT = 84
 IMG_WIDTH = 84
 MOVEMENT = RIGHT_ONLY
-
-
-# Crates a wrapper that converts the RGB image to grayscale and resizes it
-class GrayScaleAndResizeWrapper(gym.ObservationWrapper):
-    def __init__(self, env, new_width, new_height):
-        super(GrayScaleAndResizeWrapper, self).__init__(env)
-        self.observation_space = Box(low=0, high=255, shape=(1, new_height, new_width), dtype=np.uint8)
-        self.new_width = new_width
-        self.new_height = new_height
-        
-    def observation(self, obs):
-        # Greyscale
-        gray_obs = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
-
-        # Resize
-        resized_obs = cv2.resize(gray_obs, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
-        return np.expand_dims(resized_obs, axis=0)
-    
-    def render(self, mode='human'):
-        # This is the render method for the environment
-        if mode == 'rgb_array':
-            gray_frame = self.observation(self.env.render())
-            cv2.imshow("Grayscale Super Mario", gray_frame.squeeze())
-            cv2.waitKey(1)
-        else:
-            return super().render()
         
 
 def main():
@@ -48,8 +20,7 @@ def main():
     env_name = "SuperMarioBros-1-1-v0"
 
     env = gym.make(env_name, apply_api_compatibility=True, render_mode='rgb_array')
-    env = JoypadSpace(env, MOVEMENT)
-    env = GrayScaleAndResizeWrapper(env, IMG_HEIGHT, IMG_WIDTH)
+    env = apply_wrappers(env, MOVEMENT, IMG_HEIGHT, IMG_WIDTH)
 
 
     # ========================== Hiperparameters ==========================
