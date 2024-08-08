3d4c162ac2a931b02df4c7a98d03f0850aca9a43
jsancs <javivisan19@gmail.com>
2024-08-08 19:18:51 +0200

    mps support :) training on a m3 chip

--- src/ddqn/DDQN.py
+++ src/ddqn/DDQN.py
@@ -12,12 +12,15 @@ if torch.cuda.is_available():
     device = torch.device('cuda:0') 
     torch.cuda.empty_cache()
     print("Device set to : " + str(torch.cuda.get_device_name(device)))
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("Device set to : " + str(device))
 else:
     print("Device set to : cpu")
 print("==================================================================")
 
-############################## DDQN ##############################
 
+############################## DDQN ##############################
 # Create the buffer
 class ReplayBuffer:
     def __init__(self, capacity):
