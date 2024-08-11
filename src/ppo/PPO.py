f99eb9dd0ac64d537128992f42e3d4052313cde3
jsancs <javivisan19@gmail.com>
2024-08-11 17:46:32 +0200

    ppo changes

--- src/ppo/PPO.py
+++ src/ppo/PPO.py
@@ -2,14 +2,18 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.distributions import Categorical
+from tqdm import trange
 
 ################################## set device ##################################
 print("==================================================================")
 device = torch.device('cpu')
-if(torch.cuda.is_available()): 
+if torch.cuda.is_available(): 
     device = torch.device('cuda:0') 
     torch.cuda.empty_cache()
     print("Device set to : " + str(torch.cuda.get_device_name(device)))
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("Device set to : " + str(device))
 else:
     print("Device set to : cpu")
 print("==================================================================")
@@ -35,10 +39,11 @@ class RolloutBuffer:
         del self.state_values[:]
         del self.is_terminals[:]
 
+
 # Actor-Critic network
 class ActorCritic(nn.Module):
     def __init__(self, input_channels, action_dim, img_dim):
-        super(ActorCritic, self).__init__()
+        super().__init__()
 
         # Convolutional layers
         self.conv = nn.Sequential(
@@ -53,40 +58,47 @@ class ActorCritic(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        # Calculate the size of the output from conv layers
-        dummy_input = torch.zeros(1, input_channels, img_dim[0], img_dim[1])
-        conv_out_size = self.conv(dummy_input).view(dummy_input.size(0), -1).size(1)
+        
+        # # Calculate the size of the output from conv layers
+        # dummy_input = torch.zeros(1, input_channels, img_dim[0], img_dim[1])
+        # conv_out_size = self.conv(dummy_input).view(dummy_input.size(0), -1).size(1)
+        # print("Convolutional output size: ", conv_out_size); exit()
 
         # Actor network
         self.actor_fc = nn.Sequential(
-            nn.Linear(conv_out_size, 512),
-            nn.Tanh(),
+            nn.Flatten(),
+            nn.Linear(6400, 512),
+            nn.ReLU(),
             nn.Linear(512, action_dim),
             nn.Softmax(dim=-1)
         )
 
         # Critic network
         self.critic_fc = nn.Sequential(
-            nn.Linear(conv_out_size, 512),
-            nn.Tanh(),
+            nn.Flatten(),
+            nn.Linear(6400, 512),
+            nn.ReLU(),
             nn.Linear(512, 1)
         )
 
-    def forward(self):
-        raise NotImplementedError
-
-    def act(self, state):
-        # Forward pass through the convolutional layers
-        conv_out = self.conv(state).view(state.size(0), -1)
+    def forward(self, x):
+        conv_out = self.conv(x)
         action_probs = self.actor_fc(conv_out)
-        dist = Categorical(action_probs)
-
-        # Sample an action from the distribution
-        action = dist.sample()
-        action_logprob = dist.log_prob(action)
         state_val = self.critic_fc(conv_out)
+        return action_probs, state_val
 
-        return action.detach(), action_logprob.detach(), state_val.detach()
+    # def act(self, state):
+    #     # Forward pass through the convolutional layers
+    #     conv_out = self.conv(state).view(state.size(0), -1)
+    #     action_probs = self.actor_fc(conv_out)
+    #     dist = Categorical(action_probs)
+
+    #     # Sample an action from the distribution
+    #     action = dist.sample()
+    #     action_logprob = dist.log_prob(action)
+    #     state_val = self.critic_fc(conv_out)
+
+    #     return action.detach(), action_logprob.detach(), state_val.detach()
 
     def evaluate(self, old_states, old_actions):
         # Evaluate the old actions and values from the buffer
@@ -104,9 +116,10 @@ class ActorCritic(nn.Module):
         return action_logprobs, state_values, dist_entropy
 
 
+
 # PPO algorithm
 class PPO:
-    def __init__(self, input_channels, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, img_dim):
+    def __init__(self, input_channels, action_dim, lr, gamma, K_epochs, eps_clip, img_dim):
         self.gamma = gamma
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
@@ -114,87 +127,97 @@ class PPO:
         self.buffer = RolloutBuffer()
 
         self.policy = ActorCritic(input_channels, action_dim, img_dim).to(device)
-        self.optimizer = torch.optim.Adam([
-                        {'params': self.policy.actor_fc.parameters(), 'lr': lr_actor},
-                        {'params': self.policy.critic_fc.parameters(), 'lr': lr_critic}
-                    ])
+        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr)
 
         self.policy_old = ActorCritic(input_channels, action_dim, img_dim).to(device)
         self.policy_old.load_state_dict(self.policy.state_dict())
         
         self.MseLoss = nn.MSELoss()
 
+
     def select_action(self, state):
-        # Select an action from the policy
         with torch.no_grad():
-            state = torch.FloatTensor(np.array(state, copy=True)).unsqueeze(0).to(device)  # Añadir batch dimension
-            action, action_logprob, state_val = self.policy_old.act(state)
+            state = torch.FloatTensor(np.array(state, copy=True)).unsqueeze(0).to(device)
+            action_probs, state_val = self.policy(state)
+            dist = Categorical(action_probs)
+            action = dist.sample()
+            action_logprob = dist.log_prob(action)
         
-        self.buffer.states.append(state)
-        self.buffer.actions.append(action)
+        return action.item(), action_logprob
+            
+
+    def save(self, checkpoint_path):
+        torch.save(self.policy.state_dict(), checkpoint_path)
+
+
+    def load(self, checkpoint_path):
+        self.policy.load_state_dict(torch.load(checkpoint_path))
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+
+    def store_transition(self, state, action, action_logprob, reward, done):
+        self.buffer.states.append(torch.tensor(state, dtype=torch.float32))
+        self.buffer.actions.append(torch.tensor(action))
         self.buffer.logprobs.append(action_logprob)
-        self.buffer.state_values.append(state_val)
+        self.buffer.rewards.append(reward)
+        self.buffer.is_terminals.append(done)
 
-        return action.item()
+    def compute_advantages(self, rewards, values, dones, gamma, gae_lambda=0.95):
+        advantages = []
+        gae = 0
+        values = torch.cat((values, torch.tensor([0.0]).to(device)), dim=0)
 
+        for step in reversed(range(len(rewards))):
+            delta = rewards[step] + gamma * values[step + 1] * (1 - dones[step]) - values[step]
+            gae = delta + gamma * gae_lambda * (1 - dones[step]) * gae
+            advantages.insert(0, gae)
+
+        return advantages
+    
 
     def update(self):
-        # Monte Carlo estimate of returns
-        rewards = []
-        discounted_reward = 0
-        for reward, is_terminal in zip(reversed(self.buffer.rewards), reversed(self.buffer.is_terminals)):
-            if is_terminal:
-                discounted_reward = 0
-            discounted_reward = reward + (self.gamma * discounted_reward)
-            rewards.insert(0, discounted_reward)
-            
-        # Normalizing the rewards
-        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
+        states = torch.stack(self.buffer.states).to(device).detach()
+        actions = torch.stack(self.buffer.actions).to(device).detach()
+        logprobs = torch.stack(self.buffer.logprobs).to(device).detach()
+        rewards = self.buffer.rewards
+        dones = self.buffer.is_terminals
 
-        # convert list to tensor
-        old_states = torch.squeeze(torch.stack(self.buffer.states, dim=0)).detach().to(device)
-        old_actions = torch.squeeze(torch.stack(self.buffer.actions, dim=0)).detach().to(device)
-        old_logprobs = torch.squeeze(torch.stack(self.buffer.logprobs, dim=0)).detach().to(device)
-        old_state_values = torch.squeeze(torch.stack(self.buffer.state_values, dim=0)).detach().to(device)
+        # Compute the state values using the critic network
+        _, values = self.policy(states)
+        values = values.squeeze().detach()
 
-        # calculate advantages
-        advantages = rewards.detach() - old_state_values.detach()
+        # Compute the advantages
+        advantages = self.compute_advantages(rewards, values, dones, self.gamma)
+        advantages = torch.tensor(advantages, dtype=torch.float32).to(device).detach()
+
+        # Normalize the advantages
+        advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-10)
 
         # Optimize policy for K epochs
         for _ in range(self.K_epochs):
 
-            # Evaluating old actions and values
-            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+            # Evaluate actions and calculate new log_probabilities
+            action_probs, state_values = self.policy(states)
+            dist = Categorical(action_probs)
+            new_logprobs = dist.log_prob(actions)
+            entropy_loss = dist.entropy().mean()
 
-            # match state_values tensor dimensions with rewards tensor
-            state_values = torch.squeeze(state_values)
-            
-            # Finding the ratio (pi_theta / pi_theta__old)
-            ratios = torch.exp(logprobs - old_logprobs.detach())
+            # Calculate the ratio (pi_theta / pi_theta__old)
+            ratios = torch.exp(new_logprobs - logprobs.detach())
 
-            # Finding Surrogate Loss  
+            # Calculate surrogate loss
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
-
-            # final loss of clipped objective PPO
-            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
-            
-            # take gradient step
+            loss = -torch.min(surr1, surr2).mean() + 0.5 * self.MseLoss(state_values.squeeze(), torch.tensor(rewards, dtype=torch.float32).to(device)) - 0.01 * entropy_loss
+            # Optimize the policy
             self.optimizer.zero_grad()
-            loss.mean().backward()
+            loss.backward()
             self.optimizer.step()
-            
+
         # Copy new weights into old policy
         self.policy_old.load_state_dict(self.policy.state_dict())
 
-        # clear buffer
+        # Clear the buffer
         self.buffer.clear()
-    
-    def save(self, checkpoint_path):
-        torch.save(self.policy_old.state_dict(), checkpoint_path)
-   
-    def load(self, checkpoint_path):
-        self.policy_old.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
-        self.policy.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
-        
+
+        return loss.item()
--- src/ppo/train.py
+++ src/ppo/train.py
@@ -1,11 +1,12 @@
 import gym
 import cv2
+import time
 import os
 
 import wandb
 from tqdm import trange
 
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
 from src.wrappers import apply_wrappers
 from PPO import PPO
@@ -13,62 +14,77 @@ from PPO import PPO
 
 IMG_HEIGHT = 84
 IMG_WIDTH = 84
-MOVEMENT = COMPLEX_MOVEMENT
+MOVEMENT = SIMPLE_MOVEMENT
         
 
 def main():
     # Define the environment
-    env_name = "SuperMarioBrosRandomStages-v0"
+    env_name = "SuperMarioBros-1-1-v0"
 
     env = gym.make(env_name, apply_api_compatibility=True, render_mode='rgb_array')
-    env = apply_wrappers(env, MOVEMENT, IMG_HEIGHT, IMG_WIDTH, 4, 4)
+    env = apply_wrappers(env, MOVEMENT, IMG_HEIGHT, IMG_WIDTH, skip=4, stack=2)
 
 
     # ========================== Hiperparameters ==========================
+    # Define hiperparámetros
+    input_channels = 2  # Usamos 4 stacks de frames
+    action_dim = len(SIMPLE_MOVEMENT)
+    img_dim = (84, 84)
+    lr = 2.5e-4
+    gamma = 0.99
+    eps_clip = 0.2
+    k_epochs = 4
+    total_timesteps = 1_000_000_000
+    update_timestep = 2048
+    # gae_lambda = 0.95
 
-    max_ep_len = 500                      # max timesteps in one episode
-    max_training_timesteps = 100_000       # break training loop if timeteps > max_training_timesteps
+    # max_training_steps = int(5e6)     
 
-    update_timestep = max_ep_len * 5        # update policy every n timesteps
-    K_epochs = 80                           # update policy for K epochs in one PPO update
+    # # update_episode = 5                   
+    # update_steps = 2048
+    # K_epochs = 5                          
 
-    eps_clip = 0.2                          # clip parameter for PPO    
-    gamma = 0.99                            # discount factor
+    # eps_clip = 0.2                         
+    # gamma = 0.99                          
 
-    lr_actor = 0.0003                       # learning rate for actor network
-    lr_critic = 0.001                       # learning rate for critic network
+    # lr_actor = 0.0001                       
+    # lr_critic = 0.0001              
 
-    save_model_steps = 100_000            # save model every n timesteps
+    save_model_steps = 100_000          
     checkpoint_base_path = "model_checkpoints/ppo/ppo_model"
 
-    log_info_steps = 5000                    # log model info every n timesteps
+    log_info_steps = 1000       
+    log_movements_steps = 50_000
+
 
     # ========================= Enviroment ==========================
-    action_dim = env.action_space.n
-    state_dim = env.observation_space.shape
-    input_channels = state_dim[0]
+    # action_dim = env.action_space.n
+    # state_dim = env.observation_space.shape
+    # input_channels = state_dim[0]
 
 
     # ========================== Initial Hyperparameters ==========================
-    print("------- Initial Hyperparameters -------")
+    # print("------- Initial Hyperparameters -------")
 
-    print("Action Dimension: ", action_dim)
-    print("State Dimension: ", state_dim)
-    print("Input channels: ", input_channels)
+    # print("Action Dimension: ", action_dim)
+    # print("State Dimension: ", state_dim)
+    # print("Input channels: ", input_channels)
 
-    print("Max training timesteps: ", max_training_timesteps)
-    print("Max timesteps in one episode: ", max_ep_len)
-    print("Update policy every n timesteps: ", update_timestep)
-    print("Update policy for K epochs in one PPO update: ", K_epochs)
-    print("Clip parameter for PPO: ", eps_clip)
-    print("Discount factor: ", gamma)
-    print("Learning rate for actor network: ", lr_actor)
-    print("Learning rate for critic network: ", lr_critic)
+    # print("Environment: ", env_name)
+    # print("Max training episodes: ", max_training_episodes)
+    # # print("Update policy every n episodes: ", update_episode)
+    # print("Update policy every n steps: ", update_steps)
+    # print("Update policy for K epochs in one PPO update: ", K_epochs)
+    # print("Clip parameter for PPO: ", eps_clip)
+    # print("Discount factor: ", gamma)
+    # print("Learning rate for actor network: ", lr_actor)
+    # print("Learning rate for critic network: ", lr_critic)
 
-    print("--------------------------------------\n")
+    # print("--------------------------------------\n")
 
     # ========================== Model ==========================
-    ppo_agent = PPO(input_channels, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, img_dim=(IMG_HEIGHT, IMG_WIDTH))
+    # ppo_agent = PPO(input_channels, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, img_dim=(IMG_HEIGHT, IMG_WIDTH))
+    ppo_agent = PPO(input_channels, action_dim, lr, gamma, k_epochs, eps_clip, img_dim)
     os.makedirs('model_checkpoints/ppo', exist_ok=True)
 
     # Get model summary
@@ -76,74 +92,181 @@ def main():
     print(f'Total number of parameters: {total_params}')
 
     # ========================== WandB ==========================
-    wandb.init(project="mario-ppo", sync_tensorboard=True)
+    wandb.init(project="mario-ppo-final", sync_tensorboard=False)
     wandb.require("core")
     model_id = wandb.run.id
 
     # add hyperparameters to wandb
-    wandb.config.lr_actor = lr_actor
-    wandb.config.lr_critic = lr_critic
-    wandb.config.gamma = gamma
-    wandb.config.K_epochs = K_epochs
-    wandb.config.eps_clip = eps_clip
-    wandb.config.max_training_timesteps = max_training_timesteps
-    wandb.config.max_ep_len = max_ep_len
+    # wandb.config.lr_actor = lr_actor
+    # wandb.config.lr_critic = lr_critic
+    # wandb.config.gamma = gamma
+    # wandb.config.K_epochs = K_epochs
+    # wandb.config.eps_clip = eps_clip
+    # wandb.config.max_training_episodes = max_training_episodes
+    # # wandb.config.update_episode = update_episode
+    # wandb.config.update_steps = update_steps
+    # wandb.config.save_model_episodes = save_model_episodes
+    # wandb.config.log_info_episodes = log_info_episodes
+    # wandb.config.total_params = total_params
+
+    wandb.watch(ppo_agent.policy_old)
+    wandb.watch(ppo_agent.policy)
+
     wandb.config.update_timestep = update_timestep
+    wandb.config.total_timesteps = total_timesteps
     wandb.config.save_model_steps = save_model_steps
     wandb.config.log_info_steps = log_info_steps
+    wandb.config.log_movements_steps = log_movements_steps
+    wandb.config.lr = lr
+    wandb.config.gamma = gamma
+    wandb.config.eps_clip = eps_clip
+    wandb.config.k_epochs = k_epochs
+    wandb.config.img_height = IMG_HEIGHT
+    wandb.config.img_width = IMG_WIDTH
+    wandb.config.movement = MOVEMENT
 
-    # ========================== Trainig ==========================
-    time_step = 0
+    # # ========================== Trainig ==========================
+    # episode_num = 1
+    # episode_reward_mean = 0
+    # total_reward = 0
+    # total_time = 0
+    # total_steps = 0
+
+    # # while episode_num <= max_training_episodes:
+    # while total_steps <= max_training_steps:
+    #     state, _ = env.reset()
+    #     current_ep_reward = 0
+    #     done = False
+    #     episode_actions = []
+
+    #     start_ep_time = time.time()
+
+    #     while not done:
+    #         action = ppo_agent.select_action(state)
+    #         next_state, reward, done, _, __ = env.step(action)
+
+    #         ppo_agent.buffer.rewards.append(reward)
+    #         ppo_agent.buffer.is_terminals.append(done)
+
+    #         current_ep_reward += reward
+    #         episode_actions.append(action)
+            
+    #         total_steps += 1
+    #         state = next_state
+
+    #         # update PPO agent
+    #         if total_steps % update_steps == 0:
+    #             loss = ppo_agent.update()
+    #             print(f"Loss: {loss}")
+
+    #         if done:
+    #             break
+
+    #     end_ep_time = time.time()
+    #     time_per_episode = end_ep_time - start_ep_time
+    #     total_time += time_per_episode
+    #     time_avg_per_episode = total_time / episode_num
+
+    #     # save model checkpoint
+    #     if episode_num % save_model_episodes == 0:
+    #         print(f"------ Saving model checkpoint at episode {episode_num} ------")
+    #         ppo_agent.save(f"{checkpoint_base_path}_{episode_num}_{model_id}.pth")
+
+    #     # # log movements
+    #     # if episode_num % log_movements_episodes == 0:
+    #     #     wandb.log({"episode_actions": wandb.Histogram(episode_actions)})
+
+    #     # log model info
+    #     if episode_num % log_info_episodes == 0:
+    #         print(f"Model Info: Episode {episode_num}, Reward: {current_ep_reward}, Mean Reward: {episode_reward_mean}")
+
+    #     total_reward += current_ep_reward
+    #     episode_reward_mean = total_reward / episode_num
+    #     episode_num += 1
+
+
+    #     # wandb.log({
+    #     #     "episode_reward": current_ep_reward,
+    #     #     "episode_reward_mean": episode_reward_mean,
+    #     #     "episode_num": episode_num,
+    #     #     "time_per_episode": time_per_episode,
+    #     #     "time_avg_per_episode": time_avg_per_episode
+    #     # })
+
+    # env.close()
+    # cv2.destroyAllWindows()
+    # # wandb.finish()
+
+
+    timestep = 0
     episode_num = 1
     episode_reward_mean = 0
     total_reward = 0
+    total_time = 0
 
-    while time_step <= max_training_timesteps:
+    for _ in range(total_timesteps // update_timestep):
+    # while True:
         state, _ = env.reset()
-        current_ep_reward = 0
+        done = False
         episode_actions = []
+        episode_reward = 0
+        episode_time = 0
 
-        for _ in range(1, max_ep_len + 1):
-            action = ppo_agent.select_action(state)
-            state, reward, done, _, __ = env.step(action)
+        start_time = time.time()
+        while not done:
+            action, action_logprob = ppo_agent.select_action(state)
+            next_state, reward, done, _, _ = env.step(action)
 
-            ppo_agent.buffer.rewards.append(reward)
-            ppo_agent.buffer.is_terminals.append(done)
+            # store transition
+            ppo_agent.store_transition(state, action, action_logprob, reward, done)
 
-            time_step += 1
-            current_ep_reward += reward
+            # update state
+            state = next_state
+            timestep += 1
+
+            # update episode reward
+            episode_reward += reward
             episode_actions.append(action)
-            
+
+            # update PPO agent
+            if timestep % update_timestep == 0:
+                loss = ppo_agent.update()
+                wandb.log({"loss": loss})
+
             if done:
                 break
 
-            # update PPO agent
-            if time_step % update_timestep == 0:
-                ppo_agent.update() 
+            # Save model
+            if timestep % save_model_steps == 0:
+                print(f"Saving model at timestep {timestep}")
+                ppo_agent.save(f"{checkpoint_base_path}_{timestep}_{model_id}.pth")
 
-            # save model checkpoint
-            if time_step % save_model_steps == 0:
-                print(f"------ Saving model checkpoint at timestep {time_step} ------")
-                ppo_agent.save(f"{checkpoint_base_path}_{episode_num}_{model_id}.pth")
+            # Log info
+            if timestep % log_info_steps == 0:
+                print(f"Model Info: Timestep {timestep}, Reward: {episode_reward}, Mean Reward: {episode_reward_mean}")
+        
+            # Log movements
+            if timestep % log_movements_steps == 0:
+                wandb.log({"episode_actions": wandb.Histogram(episode_actions)})
 
-            # log model info
-            if time_step % log_info_steps == 0:
-                print("------ Model Info ------")
-                print(f"Time step: {time_step}")
-                print(f"Episode: {episode_num}")
-                print(f"Current Episode Reward: {current_ep_reward}")
-                print(f"Episode Reward Mean: {episode_reward_mean}")
-                print()
+        end_time = time.time()
+        episode_time = end_time - start_time
+        total_time += episode_time
+        episode_time_avg = total_time / episode_num
 
-        episode_num += 1
-        total_reward += current_ep_reward
+        total_reward += episode_reward
         episode_reward_mean = total_reward / episode_num
 
-        wandb.log({"episode_reward": current_ep_reward}, step=episode_num)
-        wandb.log({"episode_reward_mean": episode_reward_mean}, step=episode_num)
-        wandb.log({"time_step": time_step}, step=episode_num)
-        wandb.log({"actions": wandb.Histogram(episode_actions)}, step=episode_num)
+        episode_num += 1
 
+        # Log episode info
+        wandb.log({
+            "episode_reward": episode_reward,
+            "episode_reward_mean": episode_reward_mean,
+            "episode_num": episode_num,
+            "episode_time": episode_time,
+            "episode_time_avg": episode_time_avg
+        })
 
     env.close()
     cv2.destroyAllWindows()
